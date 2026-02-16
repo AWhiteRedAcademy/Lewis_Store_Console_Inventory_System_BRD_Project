b68f7c3ae24eaf64505397e70edb5be2276daead
AWhiteRedAcademy <Aidan.White@redacademy.co.za>
2026-02-16 16:33:34 +0200

    l

---
+++ Lewis_Store_Console_Inventory_System_BRD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lewis_Store_Console_Inventory_System_BRD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
