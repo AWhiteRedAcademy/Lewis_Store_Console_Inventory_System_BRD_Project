ca76aedc79662e09ca3f5ad86c1b746f0590b4d8
cassiem-source <cassssiem@gmail.com>
2026-02-17 09:43:54 +0200

    test

--- Lewis_Store_Console_Inventory_System_BRD/obj/Debug/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
+++ Lewis_Store_Console_Inventory_System_BRD/obj/Debug/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8965fc7f9600269ff6a4258fcc498ba92096e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1824c611863afea1ae70d8582d1a779e388a1aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
