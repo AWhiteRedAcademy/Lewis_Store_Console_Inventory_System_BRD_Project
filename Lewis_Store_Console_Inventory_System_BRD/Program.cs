5352222ce4f0cd6781c2654b5c99cd3f6960cd72
AWhiteRedAcademy <Aidan.White@redacademy.co.za>
2026-02-27 15:17:27 +0200

    V0.0.12

--- Lewis_Store_Console_Inventory_System_BRD/Program.cs
+++ Lewis_Store_Console_Inventory_System_BRD/Program.cs
@@ -10,12 +10,21 @@ namespace Lewis_Store_Console_Inventory_System_BRD
     {
 
 
-        public static void ItemSearch(string ItemName, string[] ItemN) 
+        public static int? ItemSearch(string ItemName, string[] ItemN) 
         {
-
+            int ItemIndex;
+            for (int i = 0; i < ItemN.Length; i++)
+            {   
+                if (ItemName.ToUpper() == ItemN[i].ToUpper())
+                {
+                    ItemIndex = i;
+                    return ItemIndex;
+                }
+            }
+            return null;
         }
 
-        public static (string Name, string Desc, int Qty, double Price) ItemAdd()
+        public static (string Name, string Desc, int Qty, decimal Price) ItemAdd()
         {
             Console.WriteLine("Item Add\n====================\nItem Name: ");
             string Name = Console.ReadLine();
@@ -27,24 +36,56 @@ namespace Lewis_Store_Console_Inventory_System_BRD
             int Qty = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Item Price 'Excl.VAT': ");
-            double Price = double.Parse(Console.ReadLine());
+            decimal Price = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Item Added Successfully");
             Console.ReadKey();
             Console.Clear();
 
             return (Name, Desc, Qty, Price);
-
-
         }
 
+        public static void DisplayStock (string[] ItemN, string[] ItemD, int[] ItemQ, decimal[] ItemP, int itemCount)
+        {
+
+            for (int i = 0; i < itemCount; i++)
+            {
+
+                Console.WriteLine($"{i}. |Item Name: {ItemN[i]}|\t|Quantity: {ItemQ[i]}|\t|Price Per Item'Excl.VAT': R{ItemP[i]}|\n|Description: {ItemD[i]}|\n" +
+                    $"===============================================\n");
+
+            }
+        }
+
+        public static void SellItem(string[] ItemN, string[] ItemD, int[] ItemQ, decimal[] ItemP, int itemCount)
+        {
+            string ItemName;
+            DisplayStock(ItemN, ItemD, ItemQ, ItemP, itemCount);
+
+            Console.WriteLine("Enter Item Name To Add To Cart, Item Name: ");
+            ItemName = Console.ReadLine();
+            if (ItemName == "") { Console.WriteLine("Error Item Does Not Exist"); goto IfError; }
+        
+
+            int? ItemIndex = ItemSearch(ItemName, ItemN);
+            if (ItemIndex == null) { Console.WriteLine("Error Item Does Not Exist"); goto IfError; }
+
+            IfError:
+            Console.WriteLine("To Checkout Type: Checkout");
+        }
+
+        public static decimal VATCAL(decimal Price)
+        {
+            decimal VAT = 0.15m;
+            return (Price + (Price * VAT));
+        }
 
         static void Main(string[] args)
         {
             string[] ItemN = new string[100];   //item name
             string[] ItemD = new string[100];   //item description
             int[] ItemQ = new int[100];         //item quantity
-            double[] ItemP = new double[100];   //item price
+            decimal[] ItemP = new decimal[100];   //item price
 
             int itemCount = 0;
             bool Continue = true;
@@ -52,7 +93,8 @@ namespace Lewis_Store_Console_Inventory_System_BRD
             while (Continue)
             {
             MenuStart:
-                Console.WriteLine("The Lewis Store Inventory Management System");
+
+                Console.WriteLine("The Lewis Store Inventory Management System\n-------------------------------------------------");
                 Console.WriteLine("Please select an option:\n1. Add Item\n2. View Inventory\n3. Sell Items\n4. Exit");
 
                 if (!int.TryParse(Console.ReadLine(), out int MenuChoice) || MenuChoice > 4)
@@ -68,13 +110,15 @@ namespace Lewis_Store_Console_Inventory_System_BRD
                     case 1:
                         {
                             Console.Clear();
-                            var Add = ItemAdd();
-                            ItemN[itemCount] = Add.Name;
-                            ItemD[itemCount] = Add.Desc;
-                            ItemQ[itemCount] = Add.Qty;
-                            ItemP[itemCount] = Add.Price;
+                            if (itemCount < 100)
+                            {
+                                var Add = ItemAdd();
+                                ItemN[itemCount] = Add.Name; ItemD[itemCount] = Add.Desc; ItemQ[itemCount] = Add.Qty; ItemP[itemCount] = Add.Price; //Adding item to parallel arrays
 
-                            itemCount++;
+                                itemCount++;
+                            }
+                            else Console.WriteLine("Error Maximium Item Count Reached No More May Be Added"); Console.Clear();
+
                             break;
                         }
 
@@ -82,18 +126,25 @@ namespace Lewis_Store_Console_Inventory_System_BRD
                         {
                             Console.Clear();
                             Console.WriteLine("View Product Stock\n===============================================\n");
-                            for (int i = 0; i < itemCount; i++)
-                            {
-
-                                Console.WriteLine($"{i}. |Item Name: {ItemN[i]}|\t|Quantity: {ItemQ[i]}|\t|Price Per Item'Excl.VAT': R{ItemP[i]}|\n|Description: {ItemD[i]}|\n" +
-                                    $"===============================================\n");
-
-                            }
-                            Console.ReadKey();
+                            DisplayStock(ItemN, ItemD, ItemQ, ItemP, itemCount);
                             break;
                         }
                     case 3:
                         {
+                            Console.Clear();
+                            
+                            Console.WriteLine("Selling Stock\n===============================================\nAdd Order To Cart? Y/N: ");
+                            if (Console.ReadLine().ToUpper() == "N") { Console.Clear(); break; }
+
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Selling Stock\n===============================================\n");
+
+                                SellItem(ItemN, ItemD, ItemQ, ItemP, itemCount);
+
+                            } while (Console.ReadLine().ToUpper() != "CHECKOUT");
+
                             Console.Clear();
 
                             break;
@@ -108,7 +159,9 @@ namespace Lewis_Store_Console_Inventory_System_BRD
 
                     default:
                         {
-
+                            Console.WriteLine("Invalid Choice, Option not available");
+                            Console.ReadKey();
+                            Console.Clear();
                             break;
                         }
                 }
--- Lewis_Store_Console_Inventory_System_BRD/obj/Release/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
+++ Lewis_Store_Console_Inventory_System_BRD/obj/Release/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lewis_Store_Console_Inventory_System_BRD")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdffe064356da24038ca5a151513151d67618b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lewis_Store_Console_Inventory_System_BRD")]
