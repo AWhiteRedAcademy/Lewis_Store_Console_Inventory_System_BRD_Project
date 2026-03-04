4ccfa11a08f2cc14f6eca9aa09dc3956839dbb0b
AWhiteRedAcademy <Aidan.White@redacademy.co.za>
2026-03-04 12:06:56 +0200

    V0.1.5

--- Lewis_Store_Console_Inventory_System_BRD/obj/Debug/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
+++ Lewis_Store_Console_Inventory_System_BRD/obj/Debug/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddaed7ee41fb8da03da2659e146e172f1cc4bd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0234bd21ff1a15d9b2013417e036e22f249f46e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
