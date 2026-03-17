465c662cce92652d05c65afd47d4975dc5f70a4f
AWhiteRedAcademy <Aidan.White@redacademy.co.za>
2026-03-17 14:27:28 +0200

    v0.0.1

---
+++ Lewis_Store_Console_Inventory_System_BRD/obj/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Item
+{
+
+    private string Name     { get { return Name; } set { Name = value; } };
+    private string Desc     { get { return Desc; } set { Desc = value } };
+    private int Quantity    { get { return Quantity; } set { Quantity = value } };
+    private decimal Price   { get { return Price; } set { Price = value } };
+
+
+    public Item(string Name, string Desc, int Qty, decimal Price)
+	{
+
+	}
+}
--- Lewis_Store_Console_Inventory_System_BRD/obj/Release/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
+++ Lewis_Store_Console_Inventory_System_BRD/obj/Release/net10.0/Lewis_Store_Console_Inventory_System_BRD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4525a4dcba304bd2fdb63f8e6f732da95ca8def6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef65cce395c2694231c9dfe9faf9dbdd82a0ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lewis_Store_Console_Inventory_System_BRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
